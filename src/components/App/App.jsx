c3dd5732d21ed20cc6c2f604e0e7e82ff7653cbf
Kirill Kurentsov <immortalsteel@gmail.com>
2023-06-14 05:52:15 +0300

    Добавлены роуты

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,12 @@ import './App.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Navigation from '../Navigation/Navigation';
+import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
 
 const App = () => {
   return (
@@ -12,6 +18,11 @@ const App = () => {
       <Navigation />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/saved-movies" element={<SavedMovies />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
       </Routes>
       <Footer />
     </div>
