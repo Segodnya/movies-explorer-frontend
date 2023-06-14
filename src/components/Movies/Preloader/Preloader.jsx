5b2e0f154c6417537c98d11c17980c6393cffd1b
Kirill Kurentsov <immortalsteel@gmail.com>
2023-06-14 05:57:00 +0300

    Добавлен код Preloader

--- src/components/Movies/Preloader/Preloader.jsx
+++ src/components/Movies/Preloader/Preloader.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import './Preloader.css';
 
 const Preloader = () => {
-  return <>Preloader</>;
+  return (
+    <div className="preloader">
+      <div className="preloader__container">
+        <span className="preloader__round"></span>
+      </div>
+    </div>
+  );
 };
 
 export default Preloader;
