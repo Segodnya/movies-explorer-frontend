0ee9702efce95112ce6acce7c483457d4021f555
Kirill Kurentsov <immortalsteel@gmail.com>
2023-06-15 12:07:53 +0300

    Добавлена секция AboutProject

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Main.css';
 import Promo from './Promo/Promo';
+import AboutProject from './AboutProject/AboutProject';
 
 const Main = () => {
   return (
     <>
       <Promo />
+      <AboutProject />
     </>
   );
 };
