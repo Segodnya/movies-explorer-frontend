4ba774856274c8d39eaf68f7fe813db92fc43bc1
Kirill Kurentsov <immortalsteel@gmail.com>
2023-06-16 12:06:46 +0300

    Add styles for Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,26 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-  return <>Footer</>;
+  return (
+    <footer className="footer">
+      <p className="footer__description">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className="footer__info">
+        <p className="footer_copyright">&copy; {`${new Date().getFullYear()}`}</p>
+        <ul className="list footer__links">
+          <li>
+            <a href="https://practicum.yandex.ru/" className="link footer__link">
+              Яндекс.Практикум
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/Segodnya/movies-explorer-frontend" className="link footer__link">
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
