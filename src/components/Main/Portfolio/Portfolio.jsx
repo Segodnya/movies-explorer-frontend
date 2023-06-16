0f1d0269db3632db4b8c954ecc38ee80e6feb9a7
Kirill Kurentsov <immortalsteel@gmail.com>
2023-06-16 11:39:58 +0300

    Add styles for AboutMe and Portfolio components

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import './Portfolio.css';
+import arrow from '../../../images/nav-arrow.svg';
 
 const Portfolio = () => {
-  return <>Portfolio</>;
+  return (
+    <div className="portfolio">
+      <h3 className="portfolio__title">Портфолио</h3>
+      <ul className="list portfolio__list">
+        <li className="portfolio__item">
+          <a href="https://segodnya.github.io/how-to-learn/" className="link portfolio__link">
+            Статичный сайт
+            <img src={arrow} alt="navigation arrow" className="portfolio__icon" />
+          </a>
+        </li>
+        <li className="portfolio__item">
+          <a href="https://segodnya.github.io/russian-travel/" className="link portfolio__link">
+            Адаптивный сайт
+            <img src={arrow} alt="navigation arrow" className="portfolio__icon" />
+          </a>
+        </li>
+        <li className="portfolio__item">
+          <a href="https://github.com/Segodnya/react-mesto-auth" className="link portfolio__link">
+            Одностраничное приложение
+            <img src={arrow} alt="navigation arrow" className="portfolio__icon" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Portfolio;
