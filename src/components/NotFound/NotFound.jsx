d6df236eef8f84d9c5659866dfcb16468c3e9a05
Kirill Kurentsov <immortalsteel@gmail.com>
2023-06-17 21:25:31 +0300

    Add styles for 404 page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
-  return <>Not Found Page</>;
+  return (
+    <section className="not-found">
+      <h1 className="not-found__title">404</h1>
+      <p className="not-found__subtitle">Страница не найдена</p>
+      <a href="/" className="link not-found__link">
+        Назад
+      </a>
+    </section>
+  );
 };
 
 export default NotFound;
