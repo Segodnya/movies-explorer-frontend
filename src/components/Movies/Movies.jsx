030b4756b290122ddfd0df66462a9085c314ec68
Kirill Kurentsov <immortalsteel@gmail.com>
2023-06-18 10:31:37 +0300

    Add styles for SearchForm

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Movies.css';
 import MoviesCardList from './MoviesCardList/MoviesCardList';
+import SearchForm from './SearchForm/SearchForm';
 
 const Movies = () => {
   return (
     <>
-      Movies
+      <SearchForm />
       <MoviesCardList />
       <button>Еще</button>
     </>
--- src/components/Movies/SearchForm/SearchForm.jsx
+++ src/components/Movies/SearchForm/SearchForm.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import './SearchForm.css';
 
 const SearchForm = () => {
-  return <>SearchForm</>;
+  return (
+    <form className="search-form">
+      <input className="search-form__input" placeholder="Фильм" />
+      <button className="search-form__button" type="submit"></button>
+    </form>
+  );
 };
 
 export default SearchForm;
