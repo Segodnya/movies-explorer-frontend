241517adea6436e1243c752c89afc2546bfd450b
Kirill Kurentsov <immortalsteel@gmail.com>
2023-06-18 11:12:55 +0300

    Add styles for FilterCheckbox

--- src/components/Movies/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/Movies/FilterCheckbox/FilterCheckbox.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import './FilterCheckbox.css';
 
 const FilterCheckbox = () => {
-  return <>FilterCheckbox</>;
+  return (
+    <form className="filter-checkbox">
+      <label className="filter-checkbox__switch">
+        <input className="filter-checkbox__input" type="checkbox" />
+        <span className="filter-checkbox__slider filter-checkbox__slider_type_round"></span>
+      </label>
+      <p className="filter-checkbox__text">Короткометражки</p>
+    </form>
+  );
 };
 
 export default FilterCheckbox;
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import './Movies.css';
 import MoviesCardList from './MoviesCardList/MoviesCardList';
 import SearchForm from './SearchForm/SearchForm';
+import FilterCheckbox from './FilterCheckbox/FilterCheckbox';
 
 const Movies = () => {
   return (
-    <>
+    <section className="movies">
       <SearchForm />
+      <FilterCheckbox />
       <MoviesCardList />
-      <button>Еще</button>
-    </>
+      <button className="movies__button">Еще</button>
+    </section>
   );
 };
 
