d104bd4eca973fca987b77888f98702505c07ff0
Kirill Kurentsov <immortalsteel@gmail.com>
2023-06-20 10:49:25 +0300

    Add form validation for Login, Register

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,11 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../../images/logo.svg';
-import './Login.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import useForm from "../../hooks/useForm";
+import logo from "../../images/logo.svg";
+import "./Login.css";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleChangeEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handleChangePassword = (e) => {
-    setPassword(e.target.value);
-  };
+  const { values, errors, handleChange, isFormValid } = useForm();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,16 +16,59 @@ const Login = () => {
       <div className="auth__container">
         <img className="auth__logo" src={logo} alt="site logo" />
         <h2 className="auth__title">Рады видеть!</h2>
-        <form name="login" className="auth__form" id="login-form" noValidate onSubmit={handleSubmit}>
+        <form
+          name="login"
+          className="auth__form"
+          id="login-form"
+          noValidate
+          onSubmit={handleSubmit}
+        >
           <label className="auth__label" htmlFor="login-email">
             E-mail
           </label>
-          <input name="login-email" className="auth__input" id="login-email-input" type="email" placeholder="E-mail" value={email} onChange={handleChangeEmail} autoComplete="off" />
+          <input
+            name="login-email"
+            className={`auth__input ${
+              errors["login-email"] ? "auth__input_errored" : ""
+            }`}
+            id="login-email-input"
+            type="email"
+            placeholder="E-mail"
+            value={values["login-email"] || ""}
+            onChange={handleChange}
+            autoComplete="off"
+            required
+          />
+          {errors["login-email"] && (
+            <span className="auth__error">{errors["login-email"]}</span>
+          )}
+
           <label className="auth__label" htmlFor="login-password">
             Пароль
           </label>
-          <input name="login-password" className="auth__input" id="login-password-input" type="password" placeholder="Пароль" value={password} onChange={handleChangePassword} autoComplete="off" />
-          <button type="submit" className="auth__button-save" id="auth-button-save-login">
+          <input
+            name="login-password"
+            className={`auth__input ${
+              errors["login-password"] ? "auth__input_errored" : ""
+            }`}
+            id="login-password-input"
+            type="password"
+            placeholder="Пароль"
+            value={values["login-password"] || ""}
+            onChange={handleChange}
+            autoComplete="off"
+            required
+          />
+          {errors["login-password"] && (
+            <span className="auth__error">{errors["login-password"]}</span>
+          )}
+
+          <button
+            type="submit"
+            className="auth__button-save"
+            id="auth-button-save-login"
+            disabled={!isFormValid}
+          >
             Войти
           </button>
         </form>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,24 +1,12 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../../images/logo.svg';
-import '../../blocks/auth/auth.css';
-import './Register.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import useForm from "../../hooks/useForm";
+import logo from "../../images/logo.svg";
+import "../../blocks/auth/auth.css";
+import "./Register.css";
 
 const Register = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleChangeName = (e) => {
-    setName(e.target.value);
-  };
-  const handleChangeEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handleChangePassword = (e) => {
-    setPassword(e.target.value);
-  };
+  const { values, errors, handleChange, isFormValid } = useForm();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -29,20 +17,77 @@ const Register = () => {
       <div className="auth__container">
         <img className="auth__logo" src={logo} alt="site logo" />
         <h2 className="auth__title">Добро пожаловать!</h2>
-        <form name="register" className="auth__form" id="register-form" noValidate onSubmit={handleSubmit}>
+        <form
+          name="register"
+          className="auth__form"
+          id="register-form"
+          noValidate
+          onSubmit={handleSubmit}
+        >
           <label className="auth__label" htmlFor="name">
             Имя
           </label>
-          <input name="name" className="auth__input" id="name-input" type="text" placeholder="Имя" value={name} onChange={handleChangeName} autoComplete="off" required minLength="2" maxLength="30" />
+          <input
+            name="name"
+            className={`auth__input ${
+              errors.name ? "auth__input_errored" : ""
+            }`}
+            id="name-input"
+            type="text"
+            placeholder="Имя"
+            value={values.name || ""}
+            onChange={handleChange}
+            autoComplete="off"
+            required
+            minLength="2"
+            maxLength="30"
+          />
+          {errors.name && <span className="auth__error">{errors.name}</span>}
+
           <label className="auth__label" htmlFor="email">
             E-mail
           </label>
-          <input name="email" className="auth__input" id="email-input" type="email" placeholder="E-mail" value={email} onChange={handleChangeEmail} autoComplete="off" required />
+          <input
+            name="email"
+            className={`auth__input ${
+              errors.email ? "auth__input_errored" : ""
+            }`}
+            id="email-input"
+            type="email"
+            placeholder="E-mail"
+            value={values.email || ""}
+            onChange={handleChange}
+            autoComplete="off"
+            required
+          />
+          {errors.email && <span className="auth__error">{errors.email}</span>}
+
           <label className="auth__label" htmlFor="password">
             Пароль
           </label>
-          <input name="password" className="auth__input" id="password-input" type="password" placeholder="Пароль" value={password} onChange={handleChangePassword} autoComplete="off" required />
-          <button type="submit" className="auth__button-save" id="auth-button-save">
+          <input
+            name="password"
+            className={`auth__input ${
+              errors.password ? "auth__input_errored" : ""
+            }`}
+            id="password-input"
+            type="password"
+            placeholder="Пароль"
+            value={values.password || ""}
+            onChange={handleChange}
+            autoComplete="off"
+            required
+          />
+          {errors.password && (
+            <span className="auth__error">{errors.password}</span>
+          )}
+
+          <button
+            type="submit"
+            className="auth__button-save"
+            id="auth-button-save"
+            disabled={!isFormValid}
+          >
             Зарегистрироваться
           </button>
         </form>
