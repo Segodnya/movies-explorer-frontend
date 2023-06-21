630ceee715c304d608a13439ee1c3c9e5b2e2036
Kirill Kurentsov <immortalsteel@gmail.com>
2023-06-21 11:55:51 +0300

    Фикс: верстка Main для 768px

--- src/components/Main/Promo/Promo.jsx
+++ src/components/Main/Promo/Promo.jsx
@@ -4,7 +4,7 @@ import "./Promo.css";
 const Promo = () => {
   return (
     <section className="promo">
-      <h1 className="title">
+      <h1 className="promo__title">
         Учебный проект студента факультета Веб-разработки.
       </h1>
       <ul className="list">
