49458ff883207f34f07a6481c51dbbaae439be4a
Kirill Kurentsov <immortalsteel@gmail.com>
2023-06-21 10:41:35 +0300

    Обнулен борден у кнопок

--- src/components/Movies/MoviesCard/MoviesCard.jsx
+++ src/components/Movies/MoviesCard/MoviesCard.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './MoviesCard.css';
+import React from "react";
+import "./MoviesCard.css";
 
 const MoviesCard = ({ title, cover, duration, isOwn }) => {
   return (
@@ -7,7 +7,11 @@ const MoviesCard = ({ title, cover, duration, isOwn }) => {
       <img className="movie-card__image" src={cover} alt="movie" />
       <div className="movie-card__description">
         <h3 className="movie-card__title">{title}</h3>
-        {isOwn ? <button className="movie-card__button movie-card__button_type_delete"></button> : <button className="movie-card__button movie-card__button_type_like"></button>}
+        {isOwn ? (
+          <button className="movie-card__button movie-card__button_type_delete"></button>
+        ) : (
+          <button className="movie-card__button movie-card__button_type_like"></button>
+        )}
       </div>
       <p className="movie-card__duration">{duration}</p>
     </article>
