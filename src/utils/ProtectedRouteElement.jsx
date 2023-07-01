ec5f45bf7cb42c58023e4948605c692ff13a3b64
Kirill Kurentsov <immortalsteel@gmail.com>
2023-07-01 13:25:28 +0300

    Убрал лишнюю логику проверки в защищенном элементе

--- src/utils/ProtectedRouteElement.jsx
+++ src/utils/ProtectedRouteElement.jsx
@@ -2,11 +2,8 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRouteElement = ({ component: Component, ...props }) => {
-  console.log(props);
-  const isUserLoaded = props.isLoggedIn || !props.isLoading;
-
   return props.isLoggedIn ? (
-    isUserLoaded && <Component {...props} replace />
+    <Component {...props} replace />
   ) : (
     <Navigate to="/signin" replace />
   );
