074b69086dcfaaa5b3b75e59c4cea305aaeae1d9
Kirill Kurentsov <immortalsteel@gmail.com>
2023-07-04 06:14:01 +0300

    Исправлен редирект со страницы 404

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './NotFound.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    navigate(-2);
+  };
+
   return (
     <main className="not-found">
       <h1 className="not-found__title">404</h1>
       <p className="not-found__subtitle">Страница не найдена</p>
-      <NavLink to="/" className="link not-found__link">
+      <NavLink to="#" className="link not-found__link" onClick={handleClick}>
         Назад
       </NavLink>
     </main>
