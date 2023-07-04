27ce22ba5c05166c00f90a0c555040f35d4a21a2
Kirill Kurentsov <immortalsteel@gmail.com>
2023-07-04 15:07:09 +0300

    При попытке перейти на любой защищённый роут происходит редирект на /

--- src/utils/ProtectedRouteElement.jsx
+++ src/utils/ProtectedRouteElement.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRouteElement = ({ component: Component, ...props }) => {
-  return props.isLoggedIn ? <Component {...props} replace /> : <Navigate to="/signin" replace />;
+  return props.isLoggedIn ? <Component {...props} replace /> : <Navigate to="/" replace />;
 };
 
 export default ProtectedRouteElement;
--- src/utils/api/mainApi.js
+++ src/utils/api/mainApi.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'https://api.sgdn.nomoredomains.rocks';
+// export const BASE_URL = 'https://api.sgdn.nomoredomains.rocks';
+export const BASE_URL = 'http://localhost:3000';
 
 const handleResponse = (res) => {
   if (res.ok) {
