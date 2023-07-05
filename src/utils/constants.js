2eb33dbc19f8f06933d9feabb8afb8bd4c18ca6b
Kirill Kurentsov <immortalsteel@gmail.com>
2023-07-05 19:56:56 +0300

    add constants for screen width values

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,4 +6,7 @@ export const MOVIES_COUNT_XS = 5;
 export const MOVIES_INCREMENT_LG = 4;
 export const MOVIES_INCREMENT_MD = 3;
 export const MOVIES_INCREMENT_SM = 2;
+export const SCREE_LG = 1174;
+export const SCREE_MD = 1023;
+export const SCREE_SM = 654;
 export const REGEX_EMAIL = '[a-z0-9]+@[a-z]+\\.[a-z]{2,5}';
